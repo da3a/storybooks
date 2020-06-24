a6887fc3e288569e87429a99d684053fadc12780
da3a <david.wallwin@gmail.com>
2020-06-24 14:42:36 +0100

    completed 1st draft of app

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const exphbs = require('express-handlebars') 
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') 
 const passport = require('passport') 
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -20,12 +21,42 @@ connectDB()
 
 const app = express()
 
+//Body Parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Method Override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+  } = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main',extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+  },
+  defaultLayout: 'main',extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 //sessions
@@ -43,6 +74,12 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+//Set Global Variable 
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null 
+  next()
+})
+
 //Static folder
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -50,6 +87,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 
 const PORT = process.env.PORT || 5000
